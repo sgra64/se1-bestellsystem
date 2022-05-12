40d479415d74a23c649de187cf1da8939552ea2d
Sven Graupner <sgraupner@beuth-hochschule.de>
2022-05-12 21:47:12 +0200

    added src/application/Application.java

---
+++ src/module-info.java
@@ -0,0 +1,16 @@
+/**
+ * Module of a simple order processing application:
+ * <code>{@value application.package_info#RootName}.</code>
+ * <p>
+ * The {@link datamodel} package exports classes for the business information
+ * (entities, relations) managed in the order processing system.
+ * </p>
+ * 
+ * @version <code style=color:green>{@value application.package_info#Version}</code>
+ * @author <code style=color:blue>{@value application.package_info#Author}</code>
+ */
+
+module se1.bestellsystem {
+	opens application;
+	exports datamodel;
+}
